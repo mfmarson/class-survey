db5c5be9d27468c7d02891f18e61f29d325f36eb
Megan <megan.marson22@gmail.com>
2024-06-09 21:31:06 -0400

    edit to javascript

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,33 @@
 "use strict";
 
+const topicsArray = ["HTML", "CSS", "JavaScript"];
+const rankingsArray = ["Awesome", "Great", "Good", "Okay", "Poor", "Unranked"];
+
 document.addEventListener("DOMContentLoaded", function () {
   console.log("DOM READY");
 
-  const form = document.getElementById("surveyForm");
-  form.addEventListener("submit", function (event) {
+  const submitButton = document.getElementById("clickSubmit");
+  submitButton.addEventListener("click", function (event) {
     event.preventDefault();
+    displayAnswer();
 
     console.log("Hello");
   });
+  function displayAnswer(event) {
+    const surveyForm = document.getElementById(surveyForm);
+
+    //get value of selected rankingsValue
+    //get value of selected topicsValue
+
+    //what do I need to get to show I am ___ at ___ on page add constants with . values and new names
+  }
 });
+
+//const variable to button -- click/eventlistener event prevent -- call back function to run what we want it to pull (aka name())
+//to get the info: .value (. are nails) so we are grabbing value attached to new variable
+//selectedOptions what the user selects [0]
+//at the very end append.child(what you want to add to the mainform)
+//
 
 // need to get value of selection from drop down
 
@@ -22,10 +40,3 @@ document.addEventListener("DOMContentLoaded", function () {
 //want to return the selection of rankings value and topics value
 
 //pull value from topics array and rankings array to create a new array
-
-// const topicsArray = ["HTML", "CSS", "JavaScript"];
-// const rankingsArray = ["Awesome", "Great", "Good", "Okay", "Poor", "Unranked"];
-
-// const topicsValue = document.getElementById("topicsValue");
-
-// const rankingsValue = document.getElementById("ratingsValue");
