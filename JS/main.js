5ef372c39bbee7f50ad0032ac49d13b6c9f06b9f
Megan <megan.marson22@gmail.com>
2024-06-13 15:19:31 -0400

    Add models, db, and main py folders

--- JS/main.js
+++ JS/main.js
@@ -15,28 +15,5 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   function displayAnswer(event) {
     const surveyForm = document.getElementById(surveyForm);
-
-    //get value of selected rankingsValue
-    //get value of selected topicsValue
-
-    //what do I need to get to show I am ___ at ___ on page add constants with . values and new names
   }
 });
-
-//const variable to button -- click/eventlistener event prevent -- call back function to run what we want it to pull (aka name())
-//to get the info: .value (. are nails) so we are grabbing value attached to new variable
-//selectedOptions what the user selects [0]
-//at the very end append.child(what you want to add to the mainform)
-//
-
-// need to get value of selection from drop down
-
-// surveyForm
-//ranking will be seen by user
-// rankingsValue--stored
-// topicsValue
-// clickSubmit
-
-//want to return the selection of rankings value and topics value
-
-//pull value from topics array and rankings array to create a new array
